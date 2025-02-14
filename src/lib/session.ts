b7335536a101e028a03825b0e538303aefd5428b
Seth@Boid.com <51824327+mchosc@users.noreply.github.com>
2025-02-14 23:29:23 +0000

    session fix

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,74 +1,72 @@
-import { configuration } from "src/lib/config";
-import { Session } from "@wharfkit/session";
-import { SessionKit } from "@wharfkit/session";
-import { WebRenderer } from "@wharfkit/web-renderer";
-import { WalletPluginAnchor } from "@wharfkit/wallet-plugin-anchor";
-import { ref, Ref } from "vue";
-import { getWorkingUrl } from "src/lib/helperFunctions";
+import { configuration } from 'src/lib/config'
+import { Session } from '@wharfkit/session'
+import { SessionKit } from '@wharfkit/session'
+import { WebRenderer } from '@wharfkit/web-renderer'
+import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor'
+import { ref, Ref } from 'vue'
+import { getWorkingUrl } from 'src/lib/helperFunctions'
 
-const session = ref<Session | undefined>(undefined) as Ref<Session | undefined>;
+const session = ref<Session | undefined>(undefined) as Ref<Session | undefined>
 
-const webRenderer = new WebRenderer();
-
-
+const webRenderer = new WebRenderer()
 
 // Resilient chains configuration
 async function getResilientChains() {
   return {
-    mainnet: {
-      id: configuration.mainnet.native.chain_id,
-      url: await getWorkingUrl(configuration.mainnet.native.apis.map(api => api.url)), // Validate URLs
-      logo: "./Telos-circle.png",
-    },
+    // mainnet: {
+    //   id: configuration.mainnet.native.chain_id,
+    //   url: await getWorkingUrl(configuration.mainnet.native.apis.map(api => api.url)), // Validate URLs
+    //   logo: "./Telos-circle.png",
+    // },
     testnet: {
       id: configuration.testnet.native.chain_id,
-      url: await getWorkingUrl(configuration.testnet.native.apis.map(api => api.url)), // Validate URLs
-      logo: "./Telos-circle.png",
+      url: await getWorkingUrl(configuration.testnet.native.apis.map((api) => api.url)), // Validate URLs
+      logo: './Telos-circle.png',
     },
-  };
+  }
 }
 
-let sessionKit: SessionKit | undefined;
+let sessionKit: SessionKit | undefined
 
 // Initialize SessionKit with resilient URLs
 async function initializeSessionKit() {
-  const chains = await getResilientChains();
+  const chains = await getResilientChains()
   sessionKit = new SessionKit({
-    appName: "BoidBridge",
+    appName: 'BoidBridge',
     chains: [
-      chains.mainnet,
+      // chains.mainnet,
       chains.testnet,
     ],
     ui: webRenderer,
     walletPlugins: [new WalletPluginAnchor()],
-  });
+  })
 }
 
 // Login
 export async function sessionLogin(): Promise<Session | undefined> {
   if (!sessionKit) {
-    await initializeSessionKit();
+    await initializeSessionKit()
   }
-  const response = await sessionKit!.login();
-  session.value = response.session;
-  return response.session;
+  const response = await sessionKit!.login()
+  session.value = response.session
+  return response.session
 }
 
 // Logout
 export async function sessionLogout(): Promise<Session | undefined> {
   if (sessionKit) {
-    await sessionKit.logout();
-    session.value = undefined;
+    await sessionKit.logout()
+    session.value = undefined
   }
-  return session.value;
+  return session.value
 }
 
 // Restore
 export async function sessionRestore(): Promise<Session | undefined> {
   if (!sessionKit) {
-    await initializeSessionKit();
+    await initializeSessionKit()
   }
-  const loginResult = await sessionKit!.restore();
-  session.value = loginResult;
-  return loginResult;
+  const loginResult = await sessionKit!.restore()
+  session.value = loginResult
+  return loginResult
 }
--- src/stores/sessionStore.ts
+++ src/stores/sessionStore.ts
@@ -1,60 +1,55 @@
-import { defineStore } from "pinia"
-import { LocalStorage } from "quasar"
-import { sessionLogin, sessionLogout, sessionRestore } from "src/lib/session"
-import { APIClient } from "@wharfkit/antelope"
-import { Session } from "@wharfkit/session";
-import { PermissionLevel } from "@wharfkit/session"
+import { defineStore } from 'pinia'
+import { LocalStorage } from 'quasar'
+import { sessionLogin, sessionLogout, sessionRestore } from 'src/lib/session'
+import { APIClient } from '@wharfkit/antelope'
+import { Session } from '@wharfkit/session'
+import { PermissionLevel } from '@wharfkit/session'
 
-export const useSessionStore = defineStore(
-  "sessionStore",
-  {
-    state: () => ({
-      session: undefined as Session | undefined,
-      multiSignToggleState: true, // to be used for toggling the multi sign modal on and off
-      clientAPI: null as APIClient | null
-    }),
-    // Getters
-    getters: {
-      isLoggedIn: (state) => state.session !== undefined,
-      username: (state) => state.session?.actor.toString() || "",
-      authorization: (state) => PermissionLevel.from(state.session?.permissionLevel as PermissionLevel || { actor: "", permission: "" }),
-      sessionState: (state) => state,
-      whatChain: (state) => state.session?.chain.name || "",
-      chainUrl: (state) => state.session?.chain.url,
-      chainLogo: (state) => state.session?.chain.getLogo() || "",
-      multiSignState: (state) => state.multiSignToggleState
+export const useSessionStore = defineStore('sessionStore', {
+  state: () => ({
+    session: undefined as Session | undefined,
+    clientAPI: null as APIClient | null,
+  }),
+  // Getters
+  getters: {
+    isLoggedIn: (state) => state.session !== undefined,
+    username: (state) => state.session?.actor.toString() || '',
+    authorization: (state) =>
+      PermissionLevel.from(
+        (state.session?.permissionLevel as PermissionLevel) || { actor: '', permission: '' },
+      ),
+    sessionState: (state) => state,
+    whatChain: (state) => state.session?.chain.name || '',
+    chainUrl: (state) => state.session?.chain.url,
+    chainLogo: (state) => state.session?.chain.getLogo() || '',
+  },
+
+  // Actions
+  actions: {
+    async login() {
+      const sessionData = await sessionLogin()
+      if (sessionData) {
+        const serializedSession = sessionData.serialize()
+        LocalStorage.set('session', serializedSession)
+        console.log('Local Session Chain URL:', sessionData.chain.url)
+      }
+      this.session = sessionData
     },
 
-    // Actions
-    actions: {
-      // Action to set the boolean property to a specific value for the multi sign modal
-      setToggleState(value:boolean) {
-        this.multiSignToggleState = value
-      },
-      async login() {
-        const sessionData = await sessionLogin()
-        if (sessionData) {
-          const serializedSession = sessionData.serialize()
-          LocalStorage.set("session", serializedSession)
-          console.log("Local Session Chain URL:", sessionData.chain.url)
-        }
-        this.session = sessionData
-      },
+    async logout() {
+      await sessionLogout()
+      LocalStorage.remove('session')
+      this.session = undefined
+    },
 
-      async logout() {
-        await sessionLogout()
-        LocalStorage.remove("session")
-        this.session = undefined
-      },
-
-      async renew() {
-        const serializedSession = LocalStorage.getItem("session")
-        if (serializedSession) {
-          this.session = await sessionRestore()
-          if (this.session) {
-            LocalStorage.set("session", this.session.serialize())
-          }
+    async renew() {
+      const serializedSession = LocalStorage.getItem('session')
+      if (serializedSession) {
+        this.session = await sessionRestore()
+        if (this.session) {
+          LocalStorage.set('session', this.session.serialize())
         }
       }
-    }
+    },
+  },
 })
